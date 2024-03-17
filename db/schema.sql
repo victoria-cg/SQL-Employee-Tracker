8c0c46d3599c4b2aea1afb90a46269786e7c96e7
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-03-17 17:32:16 -0500

    added primary and foreign keys to schema

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+-- Foreign key relationship: referring to department.id
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL FOREIGN KEY
+    FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+-- Foreign key relationship: 
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL
+    FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
+    -- Self referencing foreign key relationship that can be null will be manager_id
+    --can there be 2 foreign keys in the same table?
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
+
